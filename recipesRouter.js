de8d8fdfe43849ae8e8192d7e17c513e36917ccc
Michaela M <michaela.miller@outlook.com>
2018-08-29 19:31:51 -0600

    Testing on recipes

--- recipesRouter.js
+++ recipesRouter.js
@@ -70,12 +70,13 @@ router.put("/:id", (req, res) => {
     return res.status(400).send(message);
   }
   console.log(`Updating shopping list item \`${req.params.id}\``);
-  Recipes.update({
+  const updatedRecipe = {
     id: req.params.id,
     name: req.body.name,
     ingredients: req.body.ingredients
-  });
-  res.status(204).end();
+  }
+  Recipes.update(updatedRecipe);
+  res.status(200).json(updatedRecipe);
 });
 
 module.exports = router;
